8bd4403c34e88e3571ed2f6cfa2c47ee257422fd
JeremyShiroya <169071189+JeremyShiroya@users.noreply.github.com>
2025-12-20 10:17:36 +0300

    pwa trial

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,14 @@
 const { defineConfig } = require('@vue/cli-service')
+
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  pwa: {
+    name: 'PaletteBay',
+    themeColor: '#4DBA87',
+    msTileColor: '#000000',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black',
+    workboxPluginMode: 'GenerateSW',
+  }
 })
+
