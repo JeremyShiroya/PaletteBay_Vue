4379b4aec01db7edf4a959f743be24b4ea047e4a
JeremyShiroya <169071189+JeremyShiroya@users.noreply.github.com>
2025-12-20 10:45:59 +0300

    White changed to green

--- vue.config.js
+++ vue.config.js
@@ -4,7 +4,7 @@ module.exports = defineConfig({
   transpileDependencies: true,
   pwa: {
     name: 'PaletteBay',
-    themeColor: '#4DBA87',
+    themeColor: '#FFFFFF',
     msTileColor: '#000000',
     appleMobileWebAppCapable: 'yes',
     appleMobileWebAppStatusBarStyle: 'black',
